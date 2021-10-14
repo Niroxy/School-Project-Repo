800f7f23dfc633b35a95c795c7d788ec5e8b9fb1
Niroxy <26202334+Niroxy@users.noreply.github.com>
2021-10-14 15:36:04 +0200

    Ajout de deux fichier

---
+++ Python/Milieu.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        Calculer les cordonnées du milieu d'un parallélogramme
+# Author:      Niroxy
+#
+# Created:     14/10/2021
+# Copyright:   (c) ordi2134352 2021
+# Licence:     MIT
+#-------------------------------------------------------------------------------
+
+def main():
+    obj = str(input('Saisir Xa;Ya;Xb;Yb'))
+    v = obj.split(';')
+
+    XM = (float(v[0]) + float(v[2])) / 2
+    YM = (float(v[1]) + float(v[3])) / 2
+
+    return print('M(' + str(XM) +';' + str(YM) +')')
+    pass
+
+if __name__ == '__main__':
+    main()
